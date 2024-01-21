0764e62808ef42ecc9fa5bd4b7f5f5e4e21c2b62
Jonas <92295347+jpx40@users.noreply.github.com>
2024-01-21 18:37:12 +0100

    update

--- hypertext/src/web.rs
+++ hypertext/src/web.rs
@@ -24,3 +24,44 @@ mod axum_support {
         }
     }
 }
+
+#[cfg(feature = "actix-web")]
+mod atix_support {
+    extern crate alloc;
+    use alloc::string::String;
+
+    use actix_web_dep::{body::BoxBody, http::header, HttpRequest, HttpResponse, Responder};
+
+    use crate::Rendered;
+
+    impl<T: Into<BoxBody>> Responder for Rendered<T> {
+        type Body = String;
+
+        #[inline]
+        fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+            HttpResponse::Ok()
+                .content_type(header::ContentType::html())
+                .message_body(self.0)
+                .unwrap()
+        }
+    }
+}
+
+// #[cfg(feature = "warp")]
+// mod warp_support {
+//     extern crate alloc;
+//     use alloc::string::String;
+//
+//     use warp::{
+//         f,
+//         reply::{self, Reply, Response},
+//     };
+//
+//     use crate::Rendered;
+//     impl<T: Into<Body>> Reply for Rendered<T> {
+//         #[inline]
+//         fn into_response(self) -> Response {
+//             reply::html(self.into_string()).into_response()
+//         }
+//     }
+// }
